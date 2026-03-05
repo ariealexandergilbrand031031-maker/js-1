61c564c66a36863513845266af941e682425f93c
ariealexandergilbrand031031-maker <ariealexandergilbrand031031@gmail.com>
2026-03-05 10:57:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function cambiarPagina(){
+
+document.getElementById("mensaje").innerHTML =
+"El mejor grupo de 4to A de Informática";
+
+document.getElementById("titulo").style.color = "green";
+
+}
